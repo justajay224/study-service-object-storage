3e741a9331cc9f703ab5b3631a6064eb0ae92b95
justajay224 <ajaymaulana2224@gmail.com>
2025-06-11 12:17:59 +0700

    fix: add middleware cors

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 from route.index import include_routers
+from middleware.cors_middleware import add_cors_middleware
 import os
 import uvicorn
 from dotenv import load_dotenv
@@ -11,13 +11,7 @@ app = FastAPI()
 
 include_routers(app)
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],  
-    allow_credentials=True,
-    allow_methods=["*"],  
-    allow_headers=["*"],
-)
+add_cors_middleware(app)
 
 # @app.get("/")
 # async def root():
